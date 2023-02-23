627d0ffd0cf12318380f19b42d9736ffe100184b
Bernardo080108 <119017023+Bernardo080108@users.noreply.github.com>
2023-02-22 21:30:15 -0300

    Add files via upload

---
+++ PrimeiroComponente.js
@@ -0,0 +1,11 @@
+import filhote from './imagens/filhote.jpg';
+const PrimeiroComponente = () => {
+
+    return(
+        <div>
+              <img src = {filhote}></img>
+        </div>
+    )
+}
+
+export default PrimeiroComponente;
---
+++ Props.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Props = (props) => {
+return(
+    <div>UM exemplo de Props
+        <h1>Ele se chama : {props.name}</h1>
+   </div>
+ )
+}
+
+export default Props;
